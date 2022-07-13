5e52a0b5ae60e634ae6c5cfb3d88cfa4b312fbff
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-07-13 18:02:20 +0300

    feat: connect User and Auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './module/auth/auth.module';
+import { UsersModule } from './module/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
