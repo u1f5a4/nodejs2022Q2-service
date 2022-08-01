e8aa8b901bbb86f84d0ababa783026d3b0878698
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-08-01 14:15:13 +0300

    feat: add JwtModule

--- src/endpoints/auth/auth.module.ts
+++ src/endpoints/auth/auth.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { DBModule } from '../../module/db/db.module';
 
 @Module({
-  imports: [DBModule],
+  imports: [
+    DBModule,
+    JwtModule.register({ secret: process.env.JWT_SECRET_KEY }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
